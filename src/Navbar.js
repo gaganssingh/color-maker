a8ad5cd1b61e8c303726ea04aa5d1ad79ce74b28
Gagan Singh <gagansingh.dev@outlook.com>
2021-03-16 00:01:21 -0400

    Moved Navbar styling to /styles folder

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,9 +3,10 @@ import Slider from "rc-slider";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import { Link } from "react-router-dom";
+import { withStyles } from "@material-ui/styles";
 
 import "rc-slider/assets/index.css";
-import "./Navbar.css";
+import styles from "./styles/NavbarStyles";
 
 class Navbar extends Component {
   state = {
@@ -18,17 +19,18 @@ class Navbar extends Component {
   };
 
   render() {
-    const { level, changeLevel, showLevelSlider } = this.props;
+    const { level, changeLevel, showLevelSlider, classes } = this.props;
     const { format } = this.state;
+
     return (
-      <header className="Navbar">
-        <div className="logo">
+      <header className={classes.navbar}>
+        <div className={classes.logo}>
           <Link to="/">color maker</Link>
         </div>
         {showLevelSlider && (
-          <div className="slider-container">
+          <div>
             <span>Level: {level}</span>
-            <div className="slider">
+            <div className={classes.slider}>
               <Slider
                 defaultValue={level}
                 min={100}
@@ -39,7 +41,7 @@ class Navbar extends Component {
             </div>
           </div>
         )}
-        <div className="select-container">
+        <div className={classes.selectContainer}>
           <Select value={format} onChange={this.handleFormatChange}>
             <MenuItem value="hex">HEX - #ffffff</MenuItem>
             <MenuItem value="rgb">RGB - rgb(255, 255, 255)</MenuItem>
@@ -51,4 +53,4 @@ class Navbar extends Component {
   }
 }
 
-export default Navbar;
+export default withStyles(styles)(Navbar);
